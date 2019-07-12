59823fd039966eb9335f74536222f9ea173dc32a
Yotaro Yoshikawa <devnullyy@gmail.com>
2019-07-13 00:39:49 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleKiyoshi/ConsoleKiyoshi.cpp
@@ -0,0 +1,37 @@
+﻿// ConsoleKiyoshi.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+
+inline int ZunKiyoRandum()
+{
+	return rand() % 2;
+}
+int main()
+{
+	const std::string ZunDoko[] = { "ズン", "ドコ" };
+	std::string zundokoResult;
+	int count = 0;;
+	bool isEnd = false;
+	while (!isEnd)
+	{
+		count++;
+		int zundokoId = ZunKiyoRandum();
+		std::string zundokoValue = ZunDoko[zundokoId];
+		if (count > 5)
+		{
+			zundokoResult.erase(0, 1);
+		}
+		zundokoResult += std::to_string(zundokoId);
+
+		std::cout << zundokoValue << std::endl;
+		if (zundokoResult == "00001")
+		{
+			isEnd = true;
+			std::cout << "キ・ヨ・シ！";
+		}
+	}
+
+	return 0;
+}
